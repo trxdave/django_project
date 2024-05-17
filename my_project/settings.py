cfefba676ea4fa1bce52b4d1d196dbbf44e03f79
David Cotter <daveguy28@gmail.com>
2024-05-17 13:59:27 +0000

    Deploying to Heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-er=rvhxw^=!4-tcts8h+a^w80s)@wg#+t**8a_ce&j*hxag$9q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-trxdave-djangoproject-b7uyve30tm5.ws-eu111.gitpod.io']
+ALLOWED_HOSTS = ['8000-trxdave-djangoproject-b7uyve30tm5.ws-eu111.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
