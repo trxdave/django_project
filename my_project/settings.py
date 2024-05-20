3cc3e2394e5546cf735816043f0fd1e51fb17acf
David Cotter <daveguy28@gmail.com>
2024-05-20 09:42:10 +0000

    herokuapp update

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-er=rvhxw^=!4-tcts8h+a^w80s)@wg#+t**8a_ce&j*hxag$9q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-trxdave-djangoproject-b7uyve30tm5.ws-eu111.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-trxdave-djangoproject-b7uyve30tm5.ws-eu111.gitpod.io','.herokuapp.com']
 
 
 # Application definition
