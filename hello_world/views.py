e9eb73a95d6f703d9d41ce3e16ef4f2e788586e6
David Cotter <daveguy28@gmail.com>
2024-05-20 09:11:22 +0000

    start again and fix

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from hello_world import views as index_views
 
 # Create your views here.
+def index(request):
+
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
 
 
 def about_me(request):
-    return HttpResponse("This would be the about page")
+    return HttpResponse("Hello, world!")
--- my_project/urls.py
+++ my_project/urls.py
@@ -20,7 +20,7 @@ from hello_world import views as index_views
 from about import views as about_views
 
 urlpatterns = [
-    path('hello/', index_views.index, name='index'),
+    path('', index_views.index, name='index'),
     path('about/', about_views.about_me, name='about'),
     path('admin/', admin.site.urls),
 ]
